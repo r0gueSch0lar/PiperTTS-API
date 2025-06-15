b0608095380e42841da0aaff6c2724aa7fc8153d
foo <foo@local>
2025-06-15 10:19:09 +1000

    everything is working well enough

--- server.js
+++ server.js
@@ -3,95 +3,14 @@ const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 const { get } = require('https');
-// const JSON = require('json'); 
-// const os = require('os');
-// const cors = require('cors');
-
 const app = express();
-const PIPER_PORT = process.env.PORT || 5001;
-const DEFAULT_VOICE = "en_US-ryan-high.onnx"; // Default voice model
-// const MODELS_DIR = path.join(__dirname, 'models');
+const DEFAULT_VOICE = "en_US-ryan-high"; // Default voice model
 const MODELS_DIR = "/usr/share/piper-voices";
 const VOICES_LIST = '/usr/share/piper-voices/voices.json';
-
-// function getPiperPath() {
-//     // const platform = os.platform();
-//     // const isWSL = fs.existsSync('/proc/version') && fs.readFileSync('/proc/version', 'utf8').includes('Microsoft');
-//     // if (platform === 'win32' || (platform === 'linux' && isWSL)) {
-//     //     return './piper.exe';
-//     // } 
-//     // else if ((platform === 'linux')) {
-//     //     return 'piper-tts-bin'
-//     // } else {
-//     //     // For macOS or other platforms, set to default executable
-//     //     return './piper';
-//     // }
-//     return 'piper-tts-bin'
-// }
-    function getPiperPath() {
-        return 'piper-tts'
-    }
-// function getModelPath(fileName) {
-//     const platform = os.platform();
-//     const isWSL = fs.existsSync('/proc/version') && fs.readFileSync('/proc/version', 'utf8').includes('Microsoft');
-//     if (platform === 'win32' || (platform === 'linux' && isWSL)) {
-//         return `${fileName}`;
-//     } 
-//     else if ((platform === 'linux')) {
-//         return path.join("/usr/share/piper-voices/",fileName)
-//     } else {
-//         // For macOS or other platforms, set to default executable
-//         return path.join(__dirname,fileName);
-//     }
-// }
-    function getModelPath(fileName) {
-        return path.join("/usr/share/piper-voices/",fileName)
-    }
-// function getVoicePath(voice) {
-//     const platform = os.platform();
-//     const isWSL = fs.existsSync('/proc/version') && fs.readFileSync('/proc/version', 'utf8').includes('Microsoft');
-//     if (platform === 'win32' || (platform === 'linux' && isWSL)) {
-//         return path.join("models",voice);
-//     } else {
-//         // For macOS or other platforms, set to default executable
-//         return path.join(MODELS_DIR,voice);
-//     }
-// }
-// async function getVoicePath(voice) {
-//   try {
-//     const data = await fs.promises.readFile(VOICES_LIST, 'utf8');
-//     const voice_list = JSON.parse(data);
-//     if (
-//     //   voice_list !== {} & Array.isArray(voice_list[voice]) && voice_list[voice].length > 0 && Array.isArray(voice_list[voice]['files']) && voice_list[voice]['files'].length > 0 ){
-//       const temp = voice_list[voice]['files'][0]
-//       return  path.join(MODELS_DIR,temp);
-//     }
-//             } catch (error) {
-//     console.error('Error reading voice file:', error);
-//             }
-//         }
+const PIPER_PORT = process.env.PIPER_PORT || 5001;
+const PIPER_CORS = process.env.PIPER_CORS || false;
 
 
-// const getVoicePath = async (voice) => {
-//     await fs.readFile(VOICES_LIST, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error('Error reading file:', err);
-//             return
-//         } else {
-//             try {
-//                 // console.table(data)
-//                 // const voice_list = JSON.parse(data);
-//                 // return voice_list;
-//                 return data[voice]
-//             // return path.join(MODELS_DIR,voice_list[voice]['files'][0]);
-//             } catch (error) {
-//                 console.error('Error parsing JSON:', error);
-//                 return
-//             }
-            
-//         }
-//     })
-// }
 async function getVoicePath(voice) {
     try {
         const data = await fs.promises.readFile(VOICES_LIST, 'utf8');
@@ -107,50 +26,30 @@ async function getVoicePath(voice) {
             }
         }
         return path.join(MODELS_DIR, DEFAULT_VOICE + ".onnx");
-            // fs.access(path.join(MODELS_DIR, key), fs.constants.F_OK, (err) => {
-            //   if (err) {
-            //     console.log(voice +'File does not exist.');
-            //     return path.join(MODELS_DIR, DEFAULT_VOICE + ".onnx")
-            //   }
-            //     return path.join(MODELS_DIR, key);
-            // });
-            // if (exists(path.join(MODELS_DIR, key))) {
-            //     return path.join(MODELS_DIR, key)
-            // } else {
-            //     return path.join(MODELS_DIR, DEFAULT_VOICE + ".onnx")
-            // }
-            
     } catch (err) {
         console.error('Error parsing JSON:', err);
         return
     }
 }
 
-
-const PIPER_PATH = getPiperPath();
-
-// // Middleware 
-// app.use(cors());
-app.use(express.json());
-
- const getListOfVoices = async (dir) => {
+const getListOfVoices = async (dir) => {
     const onnxFiles = [];
     const walk = async (currentDir) => {
         try {
         const files = await fs.promises.readdir(currentDir);
         for (const file of files) {
                 try {
-            const filePath = path.join(currentDir, file);
-            const stats = await fs.promises.stat(filePath);
-            if (stats.isDirectory()) {
-                await walk(filePath);
-            } else if (path.extname(filePath) === '.onnx') {
-                onnxFiles.push(file.split(".")[0]);
-            }
+                    const filePath = path.join(currentDir, file);
+                    const stats = await fs.promises.stat(filePath);
+                    if (stats.isDirectory()) {
+                        await walk(filePath);
+                    } else if (path.extname(filePath) === '.onnx') {
+                        onnxFiles.push(file.split(".")[0]);
+                    }
                 } catch (err) {
                     console.error(`Error accessing file ${filePath}:`, err);
                     continue;
-        }
+                }       
             }
         } catch (err) {
             console.error(`Error reading directory ${currentDir}:`, err);
@@ -159,42 +58,6 @@ app.use(express.json());
     await walk(dir);
     return onnxFiles;
 };
-// // Function to get the list of voice models available
-// async function getListOfVoices(dir) {
-//     // return fs.readdirSync(MODELS_DIR).filter(file => file.endsWith('.onnx'));
-//     let voices = [];
-//     const items = await fs.promises.readdir(dir);
-
-//     for (const item of items) {
-//         const itemPath = path.join(dir, item);
-//         const stats = await fs.promises.stat(itemPath);
-//         if (stats.isDirectory()) {
-//             await getListOfVoices(itemPath);
-//         } else {
-//             if (itemPath.endsWith('.onnx')) {
-//                 voices.push(itemPath);
-//             }
-//         }
-//     }
-//     return voices;
-// }
-// async function walkDir(dir) {
-//     let voices = [];
-//     const items = await fs.promises.readdir(dir);
-
-//     for (const item of items) {
-//         const itemPath = path.join(dir, item);
-//         const stats = await fs.promises.stat(itemPath);
-//         if (stats.isDirectory()) {
-//             await walkDir(itemPath);
-//         } else {
-//             if (itemPath.endsWith('.onnx')) {
-//                 voices.push(itemPath);
-//             }
-//         }
-//     }
-//     return voices;
-// }
 
 // // Function to log the request details to a file
 function logToTextFile(text, voice) {
@@ -210,91 +73,92 @@ function generateRandomFileName() {
     return randomPart + timestampPart + '.wav';
 }
 
-// // Function to execute the Piper command with the given input and voice
-function runExecutable(input, voice, res) {
-    const tempFileName = generateRandomFileName();
-    // const outputFile = getModelPath(tempFileName);
-    const outputFile = path.join(__dirname,tempFileName);
-    
-    const voicePath = getVoicePath(voice);
-    logToTextFile(input, voice);
 
-    const cmd = `${PIPER_PATH} --model ${voicePath} --output_file ${outputFile}`;
-    console.log(cmd);
-    
-    const process = exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing command: ${stderr}`);
-            res.status(500).send('Error generating audio');
-            return;
-        }
-
-        res.setHeader('Content-Type', 'audio/wav');
-        res.setHeader('Content-Disposition', `attachment; filename="${tempFileName}"`);
-        
-        const readStream = fs.createReadStream(outputFile);
-        readStream.pipe(res);
-
-        // Clean up: remove the temporary file after sending it
-        readStream.on('end', () => {
-            fs.unlink(outputFile, (err) => {
-                if (err) console.error('Error removing temporary file:', err);
+async function runExecutable(input, voice, res) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const tempFileName = generateRandomFileName();
+            // const outputFile = path.join('/tmp', tempFileName);
+            const outputFile = path.join(__dirname, tempFileName);
+            const voicePath = await getVoicePath(voice);
+            logToTextFile(input, voice);
+            const cmd = `piper-tts --model ${voicePath} --output_file ${outputFile}`;
+            console.log(cmd);
+            const process = exec(cmd, async (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error executing command: ${stderr}`);
+                            reject(new Error('Error generating audio'));
+                    return;
+                }
+                try {
+                    res.setHeader('Content-Type', 'audio/wav');
+                    res.setHeader('Content-Disposition', `attachment; filename="${tempFileName}"`);
+                    const readStream = fs.createReadStream(outputFile);
+                    readStream.pipe(res);
+                    // Clean up: remove the temporary file after sending it
+                    readStream.on('end', () => {
+                        fs.unlink(outputFile, (err) => {
+                            if (err) console.error('Error removing temporary file:', err);
+                        });
+                        resolve();
+                    });
+                } catch (err) {
+                    console.error('Error sending response:', err);
+                    reject(new Error('Error sending response'));
+                }
             });
-        });
+            process.stdin.write(input);
+            process.stdin.end();
+        } catch (err) {
+            console.error('Error running executable:', err);
+            reject(new Error('Error running executable'));
+        }
     });
-
-    process.stdin.write(input);
-    process.stdin.end();
 }
 
 app.get('/', (req, res) => {
     res.send('Basic piper TTS server. Use /tts to convert text to speech and /voices to get available voices.');
 });
 
-// // POST request handler
-// app.post('/tts', (req, res) => {
-//     const { text, voice = DEFAULT_VOICE } = req.body;
-//     const trimmedText = text.trim();
+// POST request handler
+app.post('/tts', (req, res) => {
+    const { text, voice = DEFAULT_VOICE } = req.body;
+    const trimmedText = text.trim();
 
-//     if (!trimmedText) {
-//         return res.status(400).send('Error parsing json - text');
-//     }
-
-//     const voices = await getListOfVoices(MODELS_DIR);
-//     const selectedVoice = voices.includes(voice) ? voice : DEFAULT_VOICE;
-
-//     runExecutable(trimmedText, selectedVoice, res);
-// });
+    if (!trimmedText) {
+        return res.status(400).send('Error parsing json - text');
+    }
+    runExecutable(trimmedText, voice, res);
+});
 
 // GET request handler
 app.get('/tts', (req, res) => {
     const text = req.query.text ? req.query.text.trim() : null;
     let voice = req.query.voice || DEFAULT_VOICE;
-
     if (!text) {
         return res.status(400).send('Missing Text Parameter.');
     }
-
-    const voices = getListOfVoices();
-    if (!voices.includes(voice)) {
-        voice = DEFAULT_VOICE;
-    }
-
     runExecutable(text, voice, res);
 });
 
 // Get available voices
 app.get('/voices', async (req, res) => {
     const voices = await getListOfVoices(MODELS_DIR);
-    // const voices = await findOnnxFiles(MODELS_DIR);
     res.json(voices);
 });
 
 app.get('/test', async (req,res) => {
-    const voicepath = await getVoicePath('en_US-libritts-high');
-    res.json(voicepath)
+    // const voicepath = await getVoicePath('en_US-libritts-high');
+    // res.json(voicepath)
+    runExecutable("testing the shit out of this now", DEFAULT_VOICE, res);
 })
 
+// // Middleware 
+if (!PIPER_CORS) {
+    const cors = require('cors');
+    app.use(cors());
+}
+app.use(express.json());
 // Start the server
 app.listen(PIPER_PORT, () => {
     console.log(`Server listening on port ${PIPER_PORT}`);
